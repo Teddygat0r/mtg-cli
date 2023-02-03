d756c6b207a41615cc032871599175fa6ba1f55d
Happygator <perng.arthur@gmail.com>
2023-02-02 16:34:00 -0800

    Added general guidelines

--- Card.py
+++ Card.py
@@ -1,7 +1,23 @@
+"""
+Parts of a card:
+Name - done xd
+Mana Cost
+Typeline(supertypes and subtypes)
+Text Box
+P/T if creature
+Counters
+Tapped/Untapped
+Transformed/untransformed(daybound/nightbound, disturb, sagas, etc)
+Face up/Face down(like 3 cards in standard care about this)
+What zone it's in(for effects like cult conscript)
+
+Not relevant in standard:
+Flipped/Unflipped(only for OG kamigawa)
+"""
 class Card:
     name = "Uninitalized Card"
     def __init__(self, name):
         self.name = name
     def __str__(self):
         return self.name
-
+    
