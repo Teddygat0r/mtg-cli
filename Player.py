811636c418bc6abc3a948c3bf621ec929ab35b2f
Happygator <perng.arthur@gmail.com>
2023-04-26 17:49:09 -0700

    Partial progress on paying mana
    i didn't test this code but we don't have a production so its bing chilling

--- Player.py
+++ Player.py
@@ -44,8 +44,31 @@ class Player:
     #TODO: implement this
     #will take in a dictionary(manaCost) and prompt the player to use mana from their mana pool to satisfy requirements, will return True if cost was successfully paid and False otherwise
     def payMana(manaCost):
+        initialManaPool = self.mana_pool.copy()
+        remainingCost = manaCost.copy()
+        while remainingCost != {}:
+            print("Mana to pay:")
+            printManaPool(remainingCost)
+            print("\nYour current mana pool:")
+            printManaPool(self.mana_pool)
+            print("Enter two characters, representing the mana you will pay from your mana pool and the mana pip you will fill with that mana.")
+            a = input().split()
+            #do input here idk i lost all of my motivation
+        else:
+            return True
+            
+        self.mana_pool
         return False
-
+    
+    #Returns True or False based on whether the mana variable(a char) could be used to pay for manaPip(a string)
+    #TODO: work
+    def manaCanFit(mana, manaPip):
+        return True
+        
+    #Prints a mana pool.
+    def printManaPool(pool):
+        for i in pool:
+            print(pool[i] + " " + i + " mana")
     #def moveZone(self, obj, d)
             
     
